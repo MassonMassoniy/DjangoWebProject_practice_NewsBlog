0f5ed1ad405cd481fb4d772f75abb1c176166e52
MassonMassoniy <ageev.vassya.2@gmail.com>
2023-05-29 19:53:02 +0600

    post adding form update

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post, User
 
+class PostAdmin(admin.ModelAdmin):
+    list_display=('user', 'title', 'description', 'text', 'date_post')
+    list_filter=('user','date_post')
+
 # Register your models here
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(User)
